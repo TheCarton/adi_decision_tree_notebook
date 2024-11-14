1bdd65c0bc1fc3a0a3907edf7ea67bd8e3a35c28
TheCarton <theukearchy@gmail.com>
2024-11-14 15:23:23 -0500

    init

---
+++ shell.nix
@@ -0,0 +1,19 @@
+with import <nixpkgs> { };
+
+(pkgs.python3.withPackages (
+  ps: with ps; [
+    ipykernel
+    jupyterlab
+    matplotlib
+    numpy
+    pandas
+    seaborn
+    networkx
+    openpyxl
+    statsmodels
+    nltk
+    scikit-learn
+    nltk
+    regex  # for re functionality
+  ]
+)).env
